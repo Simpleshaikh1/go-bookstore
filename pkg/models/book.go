000f0e9f03ff2b5d64a637baff1134535c3d568a
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-05 01:03:36 +0100

    bookstore crud operation

---
+++ pkg/models/book.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"github.com/Simpleshaikh1/go-bookstore/pkg/cofig"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	cofig.Connect()
+	db = cofig.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBookById(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
